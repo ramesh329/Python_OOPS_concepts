a5032a4bb9820867165c9fc29a731951d85c60f0
Ramesh VVV <rameshvvv@Rameshs-MacBook-Air.local>
2026-01-30 22:28:09 +0530

    chatbook

--- rough.py
+++ rough.py
@@ -1,7 +1,14 @@
-my_list = [1,2,3]
-my_str = "ramesh_vellanki"
-my_int = 329
+# my_list = [1,2,3]
+# my_str = "ramesh_vellanki"
+# my_int = 329
 
 
 
-print(type(my_list))
+# print(type(my_list))
+
+# my_list.clear() #inbuilt methods. #list here is an object and clear is a method
+
+
+from oops_project import chatbook
+user1 = chatbook()
+
